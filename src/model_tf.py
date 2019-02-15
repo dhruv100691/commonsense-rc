fix 2 for gradient clipping

src/model_tf.py
@@ -59,10 +59,7 @@ class Model:
             feed_input[self.network.p_q_relation],feed_input[self.network.p_c_relation],feed_input[self.network.y] = [x for x in batch_input]
             feed_input[self.network.word_emb_mat] = self.word_emb_mat
 
-            gradients, variables = zip(*self.optimizer.compute_gradients(self.network.ce_loss))
-            gradients, _ = tf.clip_by_global_norm(gradients, self.args.grad_clipping)
-            train_op = self.optimizer.apply_gradients(zip(gradients, variables))
-            _, loss = self.sess.run([train_op, self.network.ce_loss], feed_dict=feed_input)
+            _, loss = self.sess.run([self.network.train_op, self.network.ce_loss], feed_dict=feed_input)
 
             #torch.nn.utils.clip_grad_norm(self.network.parameters(), self.args.grad_clipping)
 

src/trian_tf.py
@@ -166,7 +166,10 @@ class TriAN(object):
         with tf.name_scope("optimization"):
             # Loss function
             self.ce_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=tf.reshape(self.y,[args.batch_size,1])))
-            #self.optimizer = tf.train.AdamOptimizer(self.args.lr).minimize(self.ce_loss)
+            self.optimizer = tf.train.AdamOptimizer(self.args.lr)
+            gradients, variables = zip(*self.optimizer.compute_gradients(self.ce_loss))
+            gradients, _ = tf.clip_by_global_norm(gradients, self.args.grad_clipping)
+            self.train_op = self.optimizer.apply_gradients(zip(gradients, variables))
 
 
 